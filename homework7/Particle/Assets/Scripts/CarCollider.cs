f82ffd3779d0dea9bcd6f4e8bcb5fdacddb761fd
huanghongxun <huanghongxun2008@126.com>
2019-11-09 23:40:53 +0800

    hw7

---
+++ homework7/Particle/Assets/Scripts/CarCollider.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarCollider : MonoBehaviour
+{
+    public float damage = 0;
+    public ParticleSystem smoke;
+
+    void OnCollisionEnter(Collision collision)
+    {
+        damage += gameObject.GetComponent<Rigidbody>().velocity.magnitude;
+    }
+
+    void Update()
+    {
+        if (damage > 10)
+        {
+            var color = smoke.colorOverLifetime;
+            var gradient = new Gradient();
+            gradient.SetKeys(
+                new GradientColorKey[] { new GradientColorKey(Color.black, 0), new GradientColorKey(),  new GradientColorKey(Color.white, 1),  },
+                new GradientAlphaKey[] {  });
+            color.color = gradient;
+        }
+        else if (damage > 20)
+        {
+
+        }
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(5, 5, 100, 20), "Damage: " + damage);
+    }
+}
